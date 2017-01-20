4e2a121ec08426e60fc92708c81d8c3a1775396d
Ignacio Astorga Coto <iacfromearth@gmail.com>
2017-01-20 23:39:46 +0100

    Enemy spawn

--- Assets/Code/Radar.cs
+++ Assets/Code/Radar.cs
@@ -15,6 +15,9 @@ public class Radar : MonoBehaviour {
 	// Radio del ping
 	public float pingRadius;
 
+	// Tiempo entre cada aparición de enemigo
+	public float spawnTime;
+
 	// Tablero de enemigos
 	private List<Enemy> enemies;
 	private Transform enemiesParent;
@@ -26,6 +29,9 @@ public class Radar : MonoBehaviour {
 	// Referencia al transform del objeto
 	private Transform _transform;
 
+	// Remaining time for the next enemy to spawn
+	private float _timeRemaining = -1.0f;
+
 	private void Awake() {
 		_transform = transform;
 	}
@@ -35,20 +41,6 @@ public class Radar : MonoBehaviour {
 		enemies = new List<Enemy>();
 		enemiesParent = new GameObject("Enemies").transform;
 		enemiesParent.SetParent(_transform, false);
-
-		// TEMP: Por ahora hay enemigos en cada sitio
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				// Crea el enemigo
-				GameObject enemy = Instantiate(enemyPrefab, enemiesParent);
-				enemies.Add(enemy.GetComponent<Enemy>());
-
-				// Coloca el enemigo en su sitio (asume tamaño 1)
-				Transform enemyTransform = enemy.transform;
-				enemyTransform.localPosition = GetWorldPosition(i, j);
-				enemyTransform.localScale = enemyTransform.lossyScale;
-			}
-		}
 
 		// Crea las casillas
 		squares = new Square[size, size];
@@ -69,6 +61,32 @@ public class Radar : MonoBehaviour {
 		}
 	}
 
+	private void Update() {
+		_timeRemaining -= Time.deltaTime;
+		if (_timeRemaining < 0.0f) {
+			// Reinicia el contador
+			_timeRemaining = spawnTime;
+
+			// Selecciona una casilla aleatoria para aparecer
+			int side = Random.Range(0, 4);
+			int index = Random.Range(0, size);
+			switch (side) {
+				case 0:
+					SpawnEnemy(index, 0);
+					break;
+				case 1:
+					SpawnEnemy(index, size - 1);
+					break;
+				case 2:
+					SpawnEnemy(0, index);
+					break;
+				case 3:
+					SpawnEnemy(size - 1, index);
+					break;
+			}
+		}
+	}
+
 	public List<Enemy> GetEnemies(int x, int y) {
 		if (!IsInsideGrid(x, y))
 			return null;
@@ -81,7 +99,7 @@ public class Radar : MonoBehaviour {
 	}
 
 	public bool IsInsideGrid(int x, int y) {
-		return !(x < 0 || x >= size || y < 0 || y >= size);
+		return x >= 0 && x < size && y >= 0 && y < size;
 	}
 
 	public void Ping(int x, int y, float radius) {
@@ -95,5 +113,16 @@ public class Radar : MonoBehaviour {
 
 	private Vector3 GetWorldPosition(int x, int y) {
 		return new Vector3(x + 0.5f - size / 2.0f, y + 0.5f - size / 2.0f, 0.0f);
+	}
+
+	public void SpawnEnemy(int x, int y) {
+		// Crea el enemigo y lo emparenta
+		GameObject enemy = Instantiate(enemyPrefab, enemiesParent);
+		enemies.Add(enemy.GetComponent<Enemy>());
+
+		// Coloca el enemigo en su sitio (asume tamaño 1)
+		Transform enemyTransform = enemy.transform;
+		enemyTransform.localPosition = GetWorldPosition(x, y);
+		enemyTransform.localScale = enemyTransform.lossyScale;
 	}
 }
